12941718c1a85a275900d01569c1b19b173f30f9
LaughableStack <laughablestack@protonmail.com>
2020-10-27 14:07:51 -0400

    Add readable labels

--- src/core.js
+++ src/core.js
@@ -3,5 +3,5 @@ const http = require('http');
 const net = require('./net.js');
 const data = require('./data.js');
 
-module.exports.Duck = require('./duck.js')
-module.exports.Server = require("./swan.js")
+module.exports.APIServer = require('./duck.js')
+module.exports.WebServer = require("./swan.js")
